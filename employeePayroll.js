cd3b6ce970791904a88fd7bd01cc69a8d276895f
jaysi1701 <jaysigaur9449@gmail.com>
2026-06-11 13:13:40 +0530

    Added Employee Payroll Class with Getters and Setters

---
+++ employeePayroll.js
@@ -0,0 +1,85 @@
+class EmployeePayrollData {
+
+    constructor() {
+        this._name = "";
+        this._profilePic = "";
+        this._gender = "";
+        this._department = [];
+        this._salary = 0;
+        this._note = "";
+        this._startDate = "";
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(name) {
+        let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+
+        if (nameRegex.test(name))
+            this._name = name;
+        else
+            throw "Name is Incorrect";
+    }
+
+    get profilePic() {
+        return this._profilePic;
+    }
+
+    set profilePic(profilePic) {
+        this._profilePic = profilePic;
+    }
+
+    get gender() {
+        return this._gender;
+    }
+
+    set gender(gender) {
+        this._gender = gender;
+    }
+
+    get department() {
+        return this._department;
+    }
+
+    set department(department) {
+        this._department = department;
+    }
+
+    get salary() {
+        return this._salary;
+    }
+
+    set salary(salary) {
+        this._salary = salary;
+    }
+
+    get note() {
+        return this._note;
+    }
+
+    set note(note) {
+        this._note = note;
+    }
+
+    get startDate() {
+        return this._startDate;
+    }
+
+    set startDate(startDate) {
+        this._startDate = startDate;
+    }
+
+    toString() {
+        return (
+            "Name=" + this.name +
+            ", Profile Pic=" + this.profilePic +
+            ", Gender=" + this.gender +
+            ", Department=" + this.department +
+            ", Salary=" + this.salary +
+            ", Note=" + this.note +
+            ", Start Date=" + this.startDate
+        );
+    }
+}
